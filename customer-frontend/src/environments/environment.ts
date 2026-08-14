26c54bd6129ab3b6bdc100c5227c0fee15baef1d
Yuvraj Jain <yuvraj23jain@gmail.com>
2026-08-14 13:17:08 +0530

    chore: update adminAppUrl to HTTP S3 endpoint to bypass mixed content blocks

--- customer-frontend/src/environments/environment.ts
+++ customer-frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'http://13.204.174.51',
-  adminAppUrl: 'http://d2txxjfwitx6cr.cloudfront.net'
+  adminAppUrl: 'http://logistics-management-admin.s3-website.ap-south-1.amazonaws.com'
 };
